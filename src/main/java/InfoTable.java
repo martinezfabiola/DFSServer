826d4b6bb39e237178cb55db9e4d904948ad6b50
fabiolamartinez <fabiola.martinez@talan.com>
2019-12-09 09:46:31 -0400

    add create file method

--- src/main/java/InfoTable.java
+++ src/main/java/InfoTable.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -50,6 +53,24 @@ public class InfoTable {
 
     public synchronized void saveObject(String name, String object) {
         this.servidorInfo.objetosAlmacenados.add(name);
-        
+        this.createFile(name, object);
+    }
+
+    public void createFile(String name, String object){
+        try {
+            String fileName = name + ".json";
+            File newFile = new File(fileName);
+            if (newFile.createNewFile()) {
+              // System.out.println("File created: " + myObj.getName());
+              FileWriter myWriter = new FileWriter(fileName);
+              myWriter.write(object);
+              myWriter.close();
+            } else {
+              System.out.println("File already exists.");
+            }
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
     }
 }
